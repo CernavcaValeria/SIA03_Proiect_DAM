b35b8aeb3989a90ac6fb0cbc8493393e9480f243
ClaudiuHuludet <huludet.claudiu@gmail.com>
2021-11-23 00:56:40 +0200

    Implement reporting utilities: classes that convert a Project object to XML  /  PDF  /  CSV file

---
+++ DAMS_Proiect/DAMS_Proiect/Utilities/CSVClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAMS_Proiect.Utilities
+{
+    public static class CSVClass
+    {
+        public static bool ToCSV(this DataGridView dtDataTable, string strFilePath)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(strFilePath, false);
+
+                for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                {
+                    string colName = dtDataTable.Columns[i].HeaderText.ToString();
+                    sw.Write(colName);
+                    if (i < dtDataTable.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+                for (int i = 0; i < dtDataTable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dtDataTable.Columns.Count; j++)
+                    {
+                        Task t= Entity.project.tasksList.Find(task => task.TaskId == i + 1);
+                        if (dtDataTable.Rows[i] != null && t!=null )
+                        {
+
+                            if (dtDataTable.Rows[i].Cells["dataGridViewTextBoxColumn" + (j + 1)] != null)
+                            {
+                                var currentCellValue = dtDataTable.Rows[i].Cells["dataGridViewTextBoxColumn" + (j + 1)].Value.ToString() + ((j == 3) ? "%" : "") + ((j == 2) ? "h" : "");
+
+                                if (!Convert.IsDBNull(currentCellValue))
+                                {
+                                    string value = currentCellValue.ToString();
+                                    if (value.Contains(','))
+                                    {
+                                        value = String.Format("\"{0}\"", value);
+                                        sw.Write(value);
+                                    }
+                                    else
+                                    {
+                                        sw.Write(currentCellValue.ToString());
+                                    }
+                                }
+                                if (i < dtDataTable.Columns.Count - 1)
+                                {
+                                    sw.Write(",");
+                                }
+                            }
+                        }
+                    }
+                    sw.Write(sw.NewLine);
+                }
+                sw.Close();
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DAMS_Proiect/DAMS_Proiect/Utilities/PDFClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using System.IO;
+using Syncfusion.Drawing;
+
+namespace DAMS_Proiect
+{
+    class PDFClass
+    {
+        public void CreatePdfFile(Project currentProject,string path)
+        { 
+            PdfDocument document = new PdfDocument();
+            int nrOfPages = (int)(currentProject.tasksList.Count / 7) + 1;
+            PdfGraphics graphics;
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 11);
+
+            PdfPage[] listOfPages = new PdfPage[nrOfPages];
+            for (int  k = 1;k<=nrOfPages; k++)
+            {
+                listOfPages[k-1] = document.Pages.Add();
+                graphics = listOfPages[k - 1].Graphics;
+                graphics.DrawString(CreateProjectStatusInfo(currentProject,k), font, PdfBrushes.Black, new PointF(0, 0));
+            }
+
+            List<char> fileNamePath = new List<char>();
+            List<char> pdfFileName = new List<char>();
+            string finalName = "";
+            char c;
+            int counter = 0;
+            foreach (char ch in path)
+                fileNamePath.Add(ch);
+
+            fileNamePath.Reverse();
+            while(true)
+            {
+                c = fileNamePath[counter];
+                if (c.Equals('\\')) break;
+                counter++;
+                pdfFileName.Add(c);
+            }
+
+            pdfFileName.Reverse();           
+            foreach (char ch in pdfFileName) finalName += ch;
+
+            using (var stream = new MemoryStream())
+            {
+                document.Save(stream);               
+                File.WriteAllBytes(path, stream.ToArray());
+                stream.Close();
+            }
+            document.Close(true);
+        }
+
+
+        public string CreateProjectStatusInfo(Project currentProject,int segment)
+        {
+            StringBuilder sb = new StringBuilder();
+            Project newProject = new Project();
+            for (int i = segment * 7 - 7; i < segment * 7; i++)
+                if(i< currentProject.tasksList.Count)
+                    newProject.tasksList.Add(currentProject.tasksList[i]);
+
+            foreach (Task t in newProject.tasksList)
+            {
+                 string s = string.Format("\nTask ID={0} Information:\n\tName: {1}\n\tStart: {2}\n\tFinish: {3}\n\tDuration: {4}\n\tComplete: {5}%\n\tPriority: {6}",
+                    t.TaskId, t.Name, t.Start, t.Finish, t.Duration, t.Complete, t.Priority);
+/*
+                if (t.ResourceNames.Count > 0)
+                {
+                    Console.WriteLine("\tResource Names:");
+                    foreach (string resource in t.ResourceNames)
+                    {
+                        s += string.Format("\t\t{0}", resource);
+                    }
+                }
+*/
+                 s += "\n";
+                sb.Append(s);
+            }
+            return sb.ToString() ;
+        }
+    }
+};
