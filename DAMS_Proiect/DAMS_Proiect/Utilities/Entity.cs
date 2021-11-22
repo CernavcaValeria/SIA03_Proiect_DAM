05f7aa431df7cee73d4d0520295186ee1cdb3971
ClaudiuHuludet <huludet.claudiu@gmail.com>
2021-11-23 00:55:29 +0200

    Create class that containt main static instances of current session project, and some important flags

---
+++ DAMS_Proiect/DAMS_Proiect/Utilities/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAMS_Proiect
+{
+    public static class Entity
+    {
+        public static User user;
+        public static Project project;
+        public static Account account;
+
+        public static class Acces
+        {
+            public static bool IsUserLoggedIn = false;
+            public static bool IsFillingFromDataBase = false;
+        }
+    }
+}
